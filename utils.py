d09d981d3712db02fe16933e8834ac3b395633be
mrbusysky <58412572+mrbusysky@users.noreply.github.com>
2023-03-08 03:59:43 -0800

    examaple

--- utils.py
+++ utils.py
@@ -1,29 +1,36 @@
+import torch
+import numpy as np
+
+
 class MinMaxScaler():
     """
     Transforms each channel to the range [0, 1].
-    """    
+    """
+
     def __call__(self, tensor):
         for ch in tensor:
-            scale = 1.0 / (ch.max(dim=0)[0] - ch.min(dim=0)[0])        
-            ch.mul_(scale).sub_(ch.min(dim=0)[0])        
+            scale = 1.0 / (ch.max(dim=0)[0] - ch.min(dim=0)[0])
+            ch.mul_(scale).sub_(ch.min(dim=0)[0])
         return tensor
+
 
 def sample_noise(size, latent_dim=100):
     noise = torch.FloatTensor(size, latent_dim)
     noise.data.normal_()
     return noise
 
-def latent_space_interpolation(generator, n_samples=10, source=None, target=None):
-  if source is None and target is None:
-    random_samples = sample_noise(2, 100)
-    source = random_samples[0]
-    target = random_samples[1]
-  with torch.no_grad():
-    interpolated_z = []
-    for alpha in np.linspace(0, 1, n_samples):
-      interpolation = alpha * source + ((1 - alpha) * target)
-      interpolated_z.append(interpolation)
 
-    interpolated_z = torch.stack(interpolated_z)
-    generated_audio = generator(interpolated_z)
-  return generated_audio
+def latent_space_interpolation(generator, n_samples=10, source=None, target=None):
+    if source is None and target is None:
+        random_samples = sample_noise(2, 100)
+        source = random_samples[0]
+        target = random_samples[1]
+    with torch.no_grad():
+        interpolated_z = []
+        for alpha in np.linspace(0, 1, n_samples):
+            interpolation = alpha * source + ((1 - alpha) * target)
+            interpolated_z.append(interpolation)
+
+        interpolated_z = torch.stack(interpolated_z)
+        generated_audio = generator(interpolated_z)
+    return generated_audio
--- word2wave.py
+++ word2wave.py
@@ -2,6 +2,7 @@ import os
 import json
 import pickle
 import logging
+import numpy as np
 from urllib.request import urlretrieve
 
 import torch
@@ -11,12 +12,14 @@ from wavegan import WaveGANGenerator
 from coala import TagEncoder, AudioEncoder
 from audio_prepro import preprocess_audio
 
-logging.basicConfig(level = logging.INFO)
+logging.basicConfig(level=logging.INFO)
+
 
 class Word2Wave(nn.Module):
     def __init__(self, args):
         super(Word2Wave, self).__init__()
-        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        self.device = torch.device(
+            'cuda' if torch.cuda.is_available() else 'cpu')
         self.coala_model_name = args.coala_model_name
         self.wavegan_path = args.wavegan_path
 
@@ -26,20 +29,26 @@ class Word2Wave(nn.Module):
 
     def load_wavegan(self, slice_len=16384, model_size=32):
         path_to_model = os.path.join(self.wavegan_path)
-        self.generator = WaveGANGenerator(slice_len=slice_len, model_size=model_size, use_batch_norm=False,num_channels=1)
+        self.generator = WaveGANGenerator(
+            slice_len=slice_len, model_size=model_size, use_batch_norm=False, num_channels=1)
         checkpoint = torch.load(path_to_model, map_location=self.device)
         self.generator.load_state_dict(checkpoint['generator'])
 
-    def load_coala(self): 
-        coala_path = os.path.join("coala/models", self.coala_model_name)
-        tag_encoder_url = "https://github.com/xavierfav/coala/blob/master/saved_models/{}/tag_encoder_epoch_200.pt".format(self.coala_model_name)
-        audio_encoder_url = "https://github.com/xavierfav/coala/blob/master/saved_models/{}/audio_encoder_epoch_200.pt".format(self.coala_model_name)
-        tag_encoder_path = os.path.join(coala_path, os.path.basename(tag_encoder_url))
-        audio_encoder_path = os.path.join(coala_path, os.path.basename(audio_encoder_url))
+    def load_coala(self):
+        coala_path = os.path.join("Models", self.coala_model_name)
+        tag_encoder_url = "https://github.com/xavierfav/coala/blob/master/saved_models/{}/tag_encoder_epoch_200.pt".format(
+            self.coala_model_name)
+        audio_encoder_url = "https://github.com/xavierfav/coala/blob/master/saved_models/{}/audio_encoder_epoch_200.pt".format(
+            self.coala_model_name)
+        tag_encoder_path = os.path.join(
+            coala_path, os.path.basename(tag_encoder_url))
+        audio_encoder_path = os.path.join(
+            coala_path, os.path.basename(audio_encoder_url))
         # TODO below does not work due to corrupted download - download manually instead
         if not os.path.exists(coala_path):
             os.mkdir(coala_path)
-            logging.info("Downloading COALA model weights from {}".format(audio_encoder_url))
+            logging.info(
+                "Downloading COALA model weights from {}".format(audio_encoder_url))
             urlretrieve(tag_encoder_url, tag_encoder_path)
             urlretrieve(audio_encoder_url, audio_encoder_path)
 
@@ -51,7 +60,7 @@ class Word2Wave(nn.Module):
         self.audio_encoder.load_state_dict(torch.load(audio_encoder_path))
         self.audio_encoder.eval()
 
-        id2tag = json.load(open('coala/id2token_top_1000.json', 'rb'))
+        id2tag = json.load(open('Models/id2token_top_1000.json', 'rb'))
         self.tag2id = {tag: id for id, tag in id2tag.items()}
 
     def init_latents(self, size=1, latent_dim=100):
@@ -60,15 +69,17 @@ class Word2Wave(nn.Module):
         self.latents = torch.nn.Parameter(noise)
 
     def tokenize_text(self, text_prompt):
-        words_not_in_dict = [word for word in text_prompt.split(" ") if word not in self.tag2id.keys()]
-        words_in_dict = [word for word in text_prompt.split(" ") if word in self.tag2id.keys()]
+        words_not_in_dict = [word for word in text_prompt.split(
+            " ") if word not in self.tag2id.keys()]
+        words_in_dict = [word for word in text_prompt.split(
+            " ") if word in self.tag2id.keys()]
         tokenized_text = [int(self.tag2id[word]) for word in words_in_dict]
         return tokenized_text, words_in_dict, words_not_in_dict
 
     def encode_text(self, text_prompt):
-        word_ids,_, _ = self.tokenize_text(text_prompt)
+        word_ids, _, _ = self.tokenize_text(text_prompt)
         sentence_embedding = torch.zeros(1152).to(self.device)
-        
+
         tag_vector = torch.zeros(len(word_ids), 1000).to(self.device)
         for index, word in enumerate(word_ids):
             tag_vector[index, word] = 1
@@ -76,14 +87,15 @@ class Word2Wave(nn.Module):
         embedding, embedding_d = self.tag_encoder(tag_vector)
         sentence_embedding = embedding_d.mean(dim=0)
         return sentence_embedding
-    
+
     def encode_audio(self, audio):
         x = preprocess_audio(audio).to(self.device)
-        scaler = pickle.load(open('coala/scaler_top_1000.pkl', 'rb'))
+        scaler = pickle.load(open('Models/scaler_top_1000.pkl', 'rb'))
         x *= torch.tensor(scaler.scale_).to(self.device)
         x += torch.tensor(scaler.min_).to(self.device)
         x = torch.clamp(x, scaler.feature_range[0], scaler.feature_range[1])
-        embedding, embedding_d = self.audio_encoder(x.unsqueeze(0).unsqueeze(0))
+        embedding, embedding_d = self.audio_encoder(
+            x.unsqueeze(0).unsqueeze(0))
         return embedding_d
 
     def latent_space_interpolation(self, latents=None, n_samples=1):
@@ -109,8 +121,8 @@ class Word2Wave(nn.Module):
 
         text_embedding = text_embedding / text_embedding.norm()
         audio_embedding = audio_embedding / audio_embedding.norm()
-        
-        cos_dist =  (1 - audio_embedding @ text_embedding.t()) / 2
+
+        cos_dist = (1 - audio_embedding @ text_embedding.t()) / 2
 
         return cos_dist
 
